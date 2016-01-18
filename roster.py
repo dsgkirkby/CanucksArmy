76186b7f5f7669fa8b81df70465728e581fa9489
Dylan <dylan.pickel@gmail.com>
2016-01-17 16:39:01 -0800

    Update code style

--- roster.py
+++ roster.py
@@ -22,109 +22,120 @@ IMPERIAL = 1
 
 """ ROSTER PARSER """
 
+
 def get_player_rosters(league, season):
+    league = str(league)
+    season = str(season)
 
-	league = str(league)
-	season = str(season)
+    resultsArray = [['Name', 'Position', 'Season', 'League', 'DOB', 'Hometown', 'Height', 'Weight', 'Shoots']]
+    playerIds = []
 
-	resultsArray = [['Name','Position','Season','League','DOB','Hometown','Height','Weight','Shoots']]
-	playerIds = []
+    """ Get the league link """
 
-	""" Get the league link """
+    leagueSearchUrl = "http://www.eliteprospects.com/league_central.php"
+    leagueSearchRequest = requests.get(leagueSearchUrl)
 
-	leagueSearchUrl = "http://www.eliteprospects.com/league_central.php"
-	leagueSearchRequest = requests.get(leagueSearchUrl)
+    """ Find an <a> tag with the league name in its text """
 
-	""" Find an <a> tag with the league name in its text """
-	def league_link(tag):
-		return tag.name == 'a' and tag.text.strip().lower() == league.lower() and len(re.findall('league_home.php', tag.attrs['href'], re.IGNORECASE)) > 0
+    def league_link(tag):
+        return tag.name == 'a' and tag.text.strip().lower() == league.lower() and len(
+            re.findall('league_home.php', tag.attrs['href'], re.IGNORECASE)) > 0
 
-	leagueSearchPage = leagueSearchRequest.text.replace('<br>', '<br/>')
-	leagueSearchPage = BeautifulSoup(leagueSearchPage, "html.parser")
-	leagueLink = leagueSearchPage.find(league_link)
+    leagueSearchPage = leagueSearchRequest.text.replace('<br>', '<br/>')
+    leagueSearchPage = BeautifulSoup(leagueSearchPage, "html.parser")
+    leagueLink = leagueSearchPage.find(league_link)
 
-	""" Get the teams' links """
+    """ Get the teams' links """
 
-	def roster_title(tag):
-		return tag.name == 'div' and tag.text.strip().lower() == 'team rosters'
+    def roster_title(tag):
+        return tag.name == 'div' and tag.text.strip().lower() == 'team rosters'
 
-	teamSearchUrl = "http://www.eliteprospects.com/" + leagueLink.attrs['href'] + "&startdate=" + str(int(season) - 1)
-	teamSearchRequest = requests.get(teamSearchUrl)
+    teamSearchUrl = "http://www.eliteprospects.com/" + leagueLink.attrs['href'] + "&startdate=" + str(int(season) - 1)
+    teamSearchRequest = requests.get(teamSearchUrl)
 
-	# All tag names have this prepended to them
-	htmlPrefix = '{http://www.w3.org/1999/xhtml}'
-	teamSearchPage = html5lib.parse(teamSearchRequest.text)
-	#xpath: /html/body/div[2]/table[3]/tbody/tr/td[5]/table[5]
-	teamTable = teamSearchPage.find('./{0}body/{0}div[2]/{0}table[3]/{0}tbody/{0}tr/{0}td[5]/{0}table[5]'.format(htmlPrefix))
-	
-	teams = teamTable.findall('.//{0}tbody/{0}tr/{0}td[2]/{0}a'.format(htmlPrefix))
-	teamUrls = []
+    # All tag names have this prepended to them
+    htmlPrefix = '{http://www.w3.org/1999/xhtml}'
+    teamSearchPage = html5lib.parse(teamSearchRequest.text)
+    # xpath: /html/body/div[2]/table[3]/tbody/tr/td[5]/table[5]
+    teamTable = teamSearchPage.find(
+        './{0}body/{0}div[2]/{0}table[3]/{0}tbody/{0}tr/{0}td[5]/{0}table[5]'.format(htmlPrefix))
 
-	onFirstRow = True
+    teams = teamTable.findall('.//{0}tbody/{0}tr/{0}td[2]/{0}a'.format(htmlPrefix))
+    teamUrls = []
 
-	for team in teams:
-		if onFirstRow:
-			onFirstRow = False
-			continue
-		teamUrls.append(team.attrib['href'])
+    onFirstRow = True
 
-	""" Get the players """
+    for team in teams:
+        if onFirstRow:
+            onFirstRow = False
+            continue
+        teamUrls.append(team.attrib['href'])
 
-	for teamUrl in teamUrls:
-		teamPageRequest = requests.get('http://www.eliteprospects.com/{0}'.format(teamUrl))
-		teamPage = BeautifulSoup(teamPageRequest.text, "html.parser")
-		def global_nav(tag):
-			return tag.has_attr('id') and tag.attrs['id'] == 'globalnav'
-		playerTable = teamPage.find(global_nav).next_sibling
-		
-		players = playerTable.find_all('tr')
+    """ Get the players """
 
-		for playerIndex in range(0, len(players)):
-			""" Discard the title row """
-			if playerIndex == 0:
-				continue
-			else:
-				player = players[playerIndex]
-				playerStats = player.find_all('td')
+    for teamUrl in teamUrls:
+        teamPageRequest = requests.get('http://www.eliteprospects.com/{0}'.format(teamUrl))
+        teamPage = BeautifulSoup(teamPageRequest.text, "html.parser")
 
-				""" Only add to the array if the row isn't blank """
-				if playerStats[ID].font is not None:
-					continue
+        def global_nav(tag):
+            return tag.has_attr('id') and tag.attrs['id'] == 'globalnav'
 
-				playerId = get_player_id(playerStats)
-				if playerId in playerIds:
-					break
+        playerTable = teamPage.find(global_nav).next_sibling
 
-				playerIds.append(playerId)
-				resultsArray.append([
-					playerStats[NAME].a.text,
-					playerStats[NAME].font.text.strip()[1:-1],
-					season,
-					league,
-					playerStats[DOB].text,
-					playerStats[HOMETOWN].a.text,
-					playerStats[HEIGHT].find_all('span')[METRIC].text,
-					playerStats[WEIGHT].find_all('span')[METRIC].text,
-					playerStats[SHOOTS].text])
+        players = playerTable.find_all('tr')
 
-	with open('{0}-{1}-rosters.csv'.format(league, season), 'w', newline='') as csvFile:
-		csvWriter = csv.writer(csvFile)
-		for resultRow in resultsArray:
-			csvWriter.writerow(resultRow)
+        for playerIndex in range(0, len(players)):
+            """ Discard the title row """
+            if playerIndex == 0:
+                continue
+            else:
+                player = players[playerIndex]
+                playerStats = player.find_all('td')
 
-	print("Scraping completed successfully.")
+                """ Only add to the array if the row isn't blank """
+                if playerStats[ID].font is not None:
+                    continue
+
+                playerId = get_player_id(playerStats)
+                if playerId in playerIds:
+                    break
+
+                playerIds.append(playerId)
+                resultsArray.append([
+                    playerStats[NAME].a.text,
+                    playerStats[NAME].font.text.strip()[1:-1],
+                    season,
+                    league,
+                    playerStats[DOB].text,
+                    playerStats[HOMETOWN].a.text,
+                    playerStats[HEIGHT].find_all('span')[METRIC].text,
+                    playerStats[WEIGHT].find_all('span')[METRIC].text,
+                    playerStats[SHOOTS].text])
+
+    with open('{0}-{1}-rosters.csv'.format(league, season), 'w', newline='') as csvFile:
+        csvWriter = csv.writer(csvFile)
+        for resultRow in resultsArray:
+            csvWriter.writerow(resultRow)
+
+    print("Scraping completed successfully.")
+
 
 """ HELPER FUNCTIONS """
 
+
 def get_player_id(player):
-	return player[ID].text + player[NAME].a.text
+    return player[ID].text + player[NAME].a.text
+
 
 """ MAIN """
 
+
 def main():
-	if len(sys.argv) < 3:
-		print("Usage: expects 2 arguments - name of league (i.e. 'QMJHL') and season (end year only, i.e. '2015' for 2014-15)")
-		return
-	get_player_rosters(sys.argv[1], sys.argv[2])
+    if len(sys.argv) < 3:
+        print(
+            "Usage: expects 2 arguments - name of league (i.e. 'QMJHL') and season (end year only, i.e. '2015' for 2014-15)")
+        return
+    get_player_rosters(sys.argv[1], sys.argv[2])
+
 
 main()
--- stats.py
+++ stats.py
@@ -7,6 +7,7 @@ import re
 import sys
 import csv
 from bs4 import BeautifulSoup
+
 ID = 0
 NAME = 1
 TEAM = 2
@@ -19,83 +20,92 @@ PLUSMINUS = 9
 
 """ PLAYER STAT PARSER """
 
+
 def get_player_points(league, season):
+    league = str(league)
 
-	league = str(league)
+    resultsArray = [['Name', 'Position', 'Season', 'League', 'Team', 'GP', 'G', 'A', 'TP', 'PIM', '+/-']]
+    playerIds = []
+    pageIndex = 1
+    done = False
 
-	resultsArray = [['Name','Position','Season','League','Team','GP','G','A','TP','PIM','+/-']]
-	playerIds = []
-	pageIndex = 1
-	done = False
+    while not done:
+        url = "http://www.eliteprospects.com/league.php?currentpage={0}&season={1}&leagueid={2}".format(str(pageIndex),
+                                                                                                        str(int(
+                                                                                                            season) - 1),
+                                                                                                        league)
+        r = requests.get(url)
 
-	while not done:
-		url = "http://www.eliteprospects.com/league.php?currentpage={0}&season={1}&leagueid={2}".format(str(pageIndex), str(int(season) - 1), league)
-		r = requests.get(url)
+        regex = re.compile('PLAYER STATS')
 
-		regex = re.compile('PLAYER STATS')
+        pageText = r.text.replace('<br>', '<br/>')
 
-		pageText = r.text.replace('<br>', '<br/>')
+        soup = BeautifulSoup(pageText, "html.parser")
+        playerTable = soup.find(text=regex).parent.parent.find_all("table")[2]
 
-		soup = BeautifulSoup(pageText, "html.parser")
-		playerTable = soup.find(text=regex).parent.parent.find_all("table")[2]
+        players = playerTable.find_all('tr')
 
-		players = playerTable.find_all('tr')
+        for playerIndex in range(0, len(players)):
+            """ Discard the title row """
+            if playerIndex == 0:
+                continue
+            else:
+                player = players[playerIndex]
+                playerStats = player.find_all('td')
 
-		for playerIndex in range(0, len(players)):
-			""" Discard the title row """
-			if playerIndex == 0:
-				continue
-			else:
-				player = players[playerIndex]
-				playerStats = player.find_all('td')
+                """ Only add to the array if the row isn't blank """
+                if playerStats[NAME].a is None or '-' in playerStats[GOALS]:
+                    continue
 
-				""" Only add to the array if the row isn't blank """
-				if playerStats[NAME].a is None or '-' in playerStats[GOALS]:
-					continue
+                playerId = get_player_id(playerStats)
+                if playerId in playerIds:
+                    done = True
+                    break
 
-				playerId = get_player_id(playerStats)
-				if playerId in playerIds:
-					done = True
-					break
+                playerTeam = playerStats[TEAM].text
+                if (playerTeam == 'totals'):
+                    playerTeam = 'multiple'
 
-				playerTeam = playerStats[TEAM].text
-				if (playerTeam == 'totals'):
-					playerTeam = 'multiple'
+                playerIds.append(playerId)
+                resultsArray.append([
+                    playerStats[NAME].a.text,
+                    playerStats[NAME].font.text.strip()[1:-1],
+                    season,
+                    league,
+                    playerTeam,
+                    playerStats[GAMES].text,
+                    playerStats[GOALS].text,
+                    playerStats[ASSISTS].text,
+                    playerStats[POINTS].text,
+                    playerStats[PIM].text,
+                    playerStats[PLUSMINUS].text])
 
-				playerIds.append(playerId)
-				resultsArray.append([
-					playerStats[NAME].a.text,
-					playerStats[NAME].font.text.strip()[1:-1],
-					season,
-					league,
-					playerTeam,
-					playerStats[GAMES].text,  
-					playerStats[GOALS].text, 
-					playerStats[ASSISTS].text, 
-					playerStats[POINTS].text,
-					playerStats[PIM].text,
-					playerStats[PLUSMINUS].text])
+        pageIndex += 1
 
-		pageIndex += 1
+    with open('{0}-{1}-stats.csv'.format(league, season), 'w', newline='') as csvFile:
+        csvWriter = csv.writer(csvFile)
+        for resultRow in resultsArray:
+            csvWriter.writerow(resultRow)
 
-	with open('{0}-{1}-stats.csv'.format(league, season), 'w', newline='') as csvFile:
-		csvWriter = csv.writer(csvFile)
-		for resultRow in resultsArray:
-			csvWriter.writerow(resultRow)
+    print("Scraping completed successfully.")
 
-	print("Scraping completed successfully.")
 
 """ HELPER FUNCTIONS """
 
+
 def get_player_id(player):
-	return player[ID].text + player[NAME].a.text
+    return player[ID].text + player[NAME].a.text
+
 
 """ MAIN """
 
+
 def main():
-	if len(sys.argv) < 3:
-		print("Usage: expects 2 arguments - name of league (i.e. 'QMJHL') and season (start year only, i.e. '2015' for 2014-15)")
-		return
-	get_player_points(sys.argv[1], sys.argv[2])
+    if len(sys.argv) < 3:
+        print(
+            "Usage: expects 2 arguments - name of league (i.e. 'QMJHL') and season (start year only, i.e. '2015' for 2014-15)")
+        return
+    get_player_points(sys.argv[1], sys.argv[2])
+
 
 main()
