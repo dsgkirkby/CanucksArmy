98c0d8f86600d1c70af85c92a44a7a53fbc467c7
Dylan <dylan.pickel@gmail.com>
2016-01-23 11:27:34 -0800

    Fix encoding issues

--- helpers.py
+++ helpers.py
@@ -8,6 +8,8 @@ def export_array_to_csv(array, name):
     with open(name, 'w', newline='') as csvFile:
         csv_writer = csv.writer(csvFile)
         for resultRow in array:
+            for index, item in enumerate(resultRow):
+                resultRow[index] = str(item).replace(u'\u0148', 'n')
             csv_writer.writerow(resultRow)
 
 
