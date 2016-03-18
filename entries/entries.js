6f7632ef0030b5e4ae660ae8e6af0b8ec0dcf2a6
Dylan <dylan.pickel@gmail.com>
2016-03-17 22:36:50 -0700

    Now fully working

--- entries/entries.js
+++ entries/entries.js
@@ -2,32 +2,46 @@ var fs = require('fs');
 var http = require('http');
 var cheerio = require('cheerio');
 var csv = require('fast-csv');
+var csv_writer = require('csv-write-stream');
+var _ = require('underscore');
 
-var PERIOD = 0;
-var TIME = 1;
-var TYPE = 2;
-var PLAYER = 3;
-var NOTES = 4;
-var TMSTRENGTH = 5;
-var OPPSTRENGTH = 6;
-var WASCONTROLLED = 7;
-var FEN = 8;
-var GOAL = 9;
-var DIDFAIL = 10;
-var GAMEID = 11;
-var OPP = 12;
-var LOCATION = 13;
-var TMSCORE = 14;
-var OPPSCORE = 15;
+var PERIOD = 'Period';
+var TIME = 'Time';
+var TYPE = 'Entry Type';
+var PLAYER = 'Player';
+var NOTES = 'Notes';
+var TMSTRENGTH = 'Team strength';
+var OPPSTRENGTH = 'Opp strength';
+var WASCONTROLLED = 'Controlled?';
+var FEN = 'Fen total';
+var GOAL = 'Goal total';
+var DIDFAIL = 'Fail';
+var GAMEID = 'Game';
+var OPP = 'Opp';
+var LOCATION = 'Location';
+var TMSCORE = 'Team Score';
+var OPPSCORE = 'Opp Score';
 
 var SHIFTNUMBER = 0;
 var SHIFTPERIOD = 1;
 var SHIFTSTART = 2;
 var SHIFTEND = 3;
 
-var row = [1,0.829861111,'F','8WSH',null,5,5,null,0,0,'8WSH',20423,'FLA','HOME',0,1];
+var getEntries = fileName => {
+	return new Promise((resolve, reject) => {
+		var entries = [];
+		fs.createReadStream(fileName)
+			.pipe(csv({
+				headers: true
+			})).on('data', data => {
+				entries.push(data);
+			}).on('end', () => {
+				resolve(entries);
+			});
+	});
+};
 
-var getPage = function(url) {
+var getPage = url => {
 	return new Promise((resolve, reject) => {
 		http.get(url, response => {
 
@@ -50,14 +64,18 @@ var getPage = function(url) {
 
 var getTimeFromPercentage = p => {
 	if (isNaN(p)) {
-		return p;
+		return convertTimeToFullDigits(p);
 	} else {
 		p = parseFloat(p);
-		var minutes = Math.floor(20 * p);
+		p /= 0.833333333;
+		var minutes = Math.floor(20 * p).toString();
 		if (minutes.length < 2) {
 			minutes = '0' + minutes;
 		}
-		var seconds = Math.floor(((20 * p) % 1) * 100);
+		var seconds = Math.floor(((20 * p) % 1) * 60).toString();
+		if (seconds.length < 2) {
+			seconds = '0' + seconds;
+		}
 		return minutes + ':' + seconds;
 	}
 }
@@ -67,51 +85,125 @@ var convertTimeToFullDigits = time => {
 	return time;
 }
 
-var homeShiftPagePromise = getPage('http://www.nhl.com/scores/htmlreports/20152016/TH0' + row[GAMEID] + '.HTM')
-// var visitorShiftPagePromise = getPage('http://www.nhl.com/scores/htmlreports/20152016/TV0' + row[GAMEID] + '.HTM')
-
-homeShiftPagePromise.then(page => {
-	var $ = cheerio.load(page);
-
-	var shifts = [];
-	var $shifts = $('.oddColor, .evenColor');
-
-	var currentName;
-	var gameId = row[GAMEID];
-
-	for (var shiftId in $shifts.toArray()) {
-		var $shift = $($shifts[shiftId]);
-
-		var $data = $shift.find('td');
-		if ($data.length !== 6) {
-			continue;
+getEntries('ZoneEntries.csv').then(entries => {
+	var gameSheets = [];
+	var games = _.each(entries, entry => {
+		if (_.find(entries, entry2 => {
+			return entry[GAMEID] === entry2[GAMEID] && entry[LOCATION] === entry2[LOCATION];
+		}) === entry) {
+			gameSheets.push({
+				game: entry[GAMEID],
+				location: entry[LOCATION]
+			});
 		}
+	});	
+	
 
-		if ($($data[SHIFTNUMBER]).text() === '1') {
-			currentName = $shift.prev().prev().text()
-		}
-
-		var shiftStart = $($data[SHIFTSTART]).text();
-		var shiftEnd = $($data[SHIFTEND]).text();
-
-		shiftStart = shiftStart.substring(0, shiftStart.indexOf('/') - 1);
-		shiftEnd = shiftEnd.substring(0, shiftEnd.indexOf('/') - 1);
-
-		shifts.push({
-			name: currentName,
-			period: parseInt($($data[SHIFTPERIOD]).text()),
-			start: convertTimeToFullDigits(shiftStart),
-			end: convertTimeToFullDigits(shiftEnd),
-		});
-	}
-
-	var time = getTimeFromPercentage(row[TIME]);
-	console.log(time);
-
-	var players = shifts.filter(shift => {
-		var time = getTimeFromPercentage(row[TIME]);
-		return shift.period === row[PERIOD] && shift.start < time && shift.end > time;
+	gameSheetPromises = _.map(gameSheets, gameSheet => {
+		return getPage(
+			'http://www.nhl.com/scores/htmlreports/20152016/T' + 
+			(gameSheet.location === 'HOME' ? 'H' : 'V') + 
+			'0' + gameSheet.game + '.HTM');
 	});
 
-	console.log(players);
+	Promise.all(gameSheetPromises).then(data => {
+		var shifts = [];
+		var entryBurdens = {};
+
+		data.forEach((page, index) => {
+			var $ = cheerio.load(page);
+
+			var $shifts = $('.oddColor, .evenColor');
+
+			var currentName;
+			var gameId = gameSheets[index].game;
+
+			for (var shiftId in $shifts.toArray()) {
+				var $shift = $($shifts[shiftId]);
+
+				var $data = $shift.find('td');
+				if ($data.length !== 6) {
+					continue;
+				}
+
+				if ($($data[SHIFTNUMBER]).text() === '1') {
+					currentName = $shift.prev().prev().text()
+				}
+
+				var shiftStart = $($data[SHIFTSTART]).text();
+				var shiftEnd = $($data[SHIFTEND]).text();
+
+				shiftStart = shiftStart.substring(shiftStart.indexOf('/') + 2);
+				shiftEnd = shiftEnd.substring(shiftEnd.indexOf('/') + 2);
+
+				shifts.push({
+					name: currentName.trim(),
+					period: parseInt($($data[SHIFTPERIOD]).text()),
+					gameId: gameId,
+					start: convertTimeToFullDigits(shiftStart),
+					end: convertTimeToFullDigits(shiftEnd),
+				});
+			};
+		});
+
+		var failed = 0;
+		var successful = 0;
+
+		entries.forEach(entry => {
+			if (entry[PLAYER].indexOf('FLA') < 0) {
+				return;
+			}
+
+			var time = getTimeFromPercentage(entry[TIME]);
+
+			var players = shifts.filter(shift => {
+				return shift.period == entry[PERIOD]
+					&& shift.gameId == entry[GAMEID]
+					&& shift.start >= time
+					&& shift.end <= time;
+	        });
+
+	        var entererNumber = entry[PLAYER].substr(0, entry[PLAYER].indexOf('FLA'));
+
+	        var enterer = players.find(player => {
+	        	return player.name.substr(0, entererNumber.length) === entererNumber ? 1 : 0;
+	        });
+
+	        if (!enterer) {
+	        	failed++;
+	        	return;
+	        } else {
+	        	successful++;
+	        }
+
+			players.forEach(player => {
+				var playerDidEntry = player === enterer;
+				if (entryBurdens[player.name]) {
+					entryBurdens[player.name].push(playerDidEntry);
+				} else {
+					entryBurdens[player.name] = [playerDidEntry];
+				}
+			});
+		});
+
+		var output = []
+
+		for (var entryBurden in entryBurdens) {
+			var playerEntries = entryBurdens[entryBurden].reduce((a, b) => a + b);
+			var totalEntries = entryBurdens[entryBurden].length;
+			output.push({
+				'Player Name': entryBurden,
+				'Player Entries': playerEntries,
+				'On Ice Total': totalEntries,
+				'Success Percentage': (playerEntries / totalEntries * 100).toFixed(2) + '%'
+			});
+		}
+
+		var writer = csv_writer();
+		writer.pipe(fs.createWriteStream('entries.csv'));
+		output.forEach(line => {
+			writer.write(line);
+		});
+		writer.end();
+	});
 });
