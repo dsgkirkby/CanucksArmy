08cb917d22da1dfa379326020ae1662082a5c3e9
Dylan <dylan.pickel@gmail.com>
2016-05-18 19:41:16 -0700

    Add birth date option to draft scraper

--- draft.py
+++ draft.py
@@ -7,6 +7,7 @@ def main():
     arg_parser = argparse.ArgumentParser(description="Get draft picks from the specified NHL draft")
     arg_parser.add_argument('start_season', type=int, help="Earliest draft to scrape")
     arg_parser.add_argument('--range', type=int, help="Latest draft to scrape", required=False)
+    arg_parser.add_argument('--extra', action='store_true', help="Whether to show extra information about each player (significantly slower)")
 
     args = arg_parser.parse_args()
 
@@ -16,7 +17,7 @@ def main():
     results_array = []
 
     for season in range(start_season, end_season + 1):
-        results_array = draft.get_draft_picks(season, results_array)
+        results_array = draft.get_draft_picks(season, results_array, args.extra)
 
     helpers.export_array_to_csv(results_array, 'nhl_draft_{0}-{1}.csv'.format(start_season, end_season))
 
--- modules/draft.py
+++ modules/draft.py
@@ -13,11 +13,11 @@ POINTS = 7
 PIM = 8
 
 
-def get_draft_picks(season, results_array=None):
+def get_draft_picks(season, results_array=None, show_extra=False):
     if results_array is None:
         results_array = []
     if len(results_array) == 0:
-        results_array.append(['Year', 'Round', 'Number', 'Team', 'Name', 'Position', 'Seasons', 'Games', 'Goals', 'Assists', 'Points', 'PIM'])
+        results_array.append(['Year', 'Round', 'Number', 'Team', 'Name', 'Position', 'Seasons', 'Games', 'Goals', 'Assists', 'Points', 'PIM', 'Birthday'])
 
     url = 'http://www.eliteprospects.com/draft.php?year={0}'.format(str(season))
     r = requests.get(url)
@@ -44,10 +44,18 @@ def get_draft_picks(season, results_array=None):
                 round_number += 1
             continue
 
+        player_birthday = ''
+
         try:
             name_link = columns[NAME].find('{0}a'.format(html_prefix))
             name = name_link.text.strip()
             position = name_link.find('{0}font'.format(html_prefix)).text.strip()[1:-1]
+
+            if show_extra:
+                player_request = requests.get('http://www.eliteprospects.com/' + name_link.attrib['href'])
+                player_page = html5lib.parse(player_request.text)
+
+                player_birthday = player_page.find('./{0}body/{0}div[2]/{0}table[3]/{0}tbody/{0}tr/{0}td[5]/{0}p/{0}table[2]/{0}tbody/{0}tr/{0}td[1]/{0}table/{0}tbody/{0}tr[1]/{0}td[2]/{0}a'.format(html_prefix)).text
         except AttributeError:
             name = 'No selection made'
             position = 'N/A'
@@ -64,7 +72,8 @@ def get_draft_picks(season, results_array=None):
             columns[GOALS].text or '',
             columns[ASSISTS].text or '',
             columns[POINTS].text or '',
-            columns[PIM].text or ''
+            columns[PIM].text or '',
+            player_birthday
         ])
 
         pick_number += 1
