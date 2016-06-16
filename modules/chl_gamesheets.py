c21a2532cdb94df1828e501fb1032c08f2e791f1
Dylan <dylan.pickel@gmail.com>
2016-06-15 21:29:53 -0700

    Output player names in addition to their numbers

--- modules/chl_gamesheets.py
+++ modules/chl_gamesheets.py
@@ -10,6 +10,19 @@ def find_from_xpath(root, xpath):
     return root.find(xpath_modified)
 
 
+def get_player_name(root, number, is_away):
+    roster_table = find_from_xpath(root, './body/table[1]/tbody/tr[3]/td[{0}]/table/tbody'.format('1' if is_away else '3'))
+    for player in roster_table[2:]:
+        if int(player[1].text) == int(number):
+            bold = find_from_xpath(player[2], './b')
+            if find_from_xpath(player[2], './b') is not None:
+                name = bold.text
+            else:
+                name = player[2].text
+            return name.split(',')[1].strip() + ' ' + name.split(',')[0].strip() + '-' + number
+    return number
+
+
 def scrape_gamesheets(game_ids, url):
     goals = [['GameID', 'date', 'visiting team', 'home team', 'GF team', 'GA team', 'period', 'time', 'scorer', 'assist1', 'assist2', 'situation', 'plus', 'minus']]
 
@@ -31,32 +44,31 @@ def scrape_gamesheets(game_ids, url):
             if (len(row[0].text.strip())) < 1:
                 continue
 
-            if row in away_goals_table[2:]:
-                gf_team = visiting_team
-                ga_team = home_team
-            else:
-                gf_team = home_team
-                ga_team = visiting_team
+            is_away = row in away_goals_table[2:]
 
             time = row[1].text.strip()
             if time[0] == ':':
                 time = '0' + time
+
+            def parse_plus_minus(player_number_list, is_away_local):
+                non_empty_list = filter(lambda x: len(x.strip()) > 0, map(lambda x: x.text, player_number_list))
+                return ','.join(list(map(lambda x: get_player_name(game_report, x, is_away_local), non_empty_list)))
 
             goals.append([
                 game_id,
                 date,
                 visiting_team,
                 home_team,
-                gf_team,
-                ga_team,
+                visiting_team if is_away else home_team,
+                home_team if is_away else visiting_team,
                 row[0].text.strip(),
                 time,
-                row[3].text.strip().split('-')[0],
-                row[3].text.strip().split('-')[1] if len(row[3].text.split('-')) > 1 else '',
-                row[3].text.strip().split('-')[2] if len(row[3].text.split('-')) > 2 else '',
+                get_player_name(game_report, row[3].text.strip().split('-')[0], is_away),
+                get_player_name(game_report, row[3].text.strip().split('-')[1], is_away) if len(row[3].text.split('-')) > 1 else '',
+                get_player_name(game_report, row[3].text.strip().split('-')[2], is_away) if len(row[3].text.split('-')) > 2 else '',
                 '' if row[2].text is None else row[2].text.strip(),
-                ','.join(list(filter(lambda x: len(x.strip()) > 0, map(lambda x: x.text, row[4:10])))),
-                ','.join(list(filter(lambda x: len(x.strip()) > 0, map(lambda x: x.text, row[10:16])))),
+                parse_plus_minus(row[4:10], is_away),
+                parse_plus_minus(row[10:16], not is_away),
             ])
 
         print('{0}/{1}'.format(str(game_ids.index(game_id) + 1), str(len(game_ids))))
