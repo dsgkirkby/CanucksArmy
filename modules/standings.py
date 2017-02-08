029d46e4ba06dc77cb42d049feb4ebdf5734a404
Dylan <dylan.pickel@gmail.com>
2017-02-07 18:05:29 -0800

    Count OT Wins as Wins and group Ties and OT Losses under an OT column

--- modules/standings.py
+++ modules/standings.py
@@ -4,6 +4,7 @@ import html5lib
 NAME = 1
 GAMES = 2
 WINS = 3
+TIES = 4
 LOSSES = 5
 OT_WINS = 6
 OT_LOSSES = 7
@@ -20,7 +21,7 @@ def get_league_standings(league, season, results_array=None):
     season = str(season)
 
     if results_array is None or len(results_array) == 0:
-        results_array.append(['Team Name', 'League', 'Season', 'Games', 'W', 'L', 'OTW', 'OTL', 'GF', 'GA', 'Points', 'PostSeason'])
+        results_array.append(['Team Name', 'League', 'Season', 'Games', 'W', 'L', 'OT', 'GF', 'GA', 'Points', 'PostSeason'])
 
     standings_url = 'http://www.eliteprospects.com/standings.php?league={0}&startdate={1}'.format(league, str(int(season) - 1))
     request = requests.get(standings_url)
@@ -52,10 +53,18 @@ def get_league_standings(league, season, results_array=None):
 
         name = team_stats[NAME].find('.//{0}a'.format(html_prefix)).text
         games = team_stats[GAMES].text
-        wins = team_stats[WINS].text
-        losses = team_stats[LOSSES].text
-        ot_wins = team_stats[OT_WINS].text
-        ot_losses = team_stats[OT_LOSSES].text
+
+        def parse_number(node):
+            try:
+                return int(node.text)
+            except (ValueError, Exception):
+                return 0
+
+        wins = parse_number(team_stats[WINS])
+        losses = parse_number(team_stats[LOSSES])
+        ties = parse_number(team_stats[TIES])
+        ot_wins = parse_number(team_stats[OT_WINS])
+        ot_losses = parse_number(team_stats[OT_LOSSES])
         goals_for = team_stats[GOALS_FOR].text
         goals_against = team_stats[GOALS_AGAINST].text
         points = team_stats[POINTS].find('.//{0}strong'.format(html_prefix)).text
@@ -66,10 +75,9 @@ def get_league_standings(league, season, results_array=None):
             league,
             season,
             games,
-            wins,
+            wins + ot_wins,
             losses,
-            ot_wins,
-            ot_losses,
+            ot_losses + ties,
             goals_for,
             goals_against,
             points,
