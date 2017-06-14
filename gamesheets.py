43cf48e44225f299c7af023a3383072ea016757b
Dylan <dylan.pickel@gmail.com>
2017-06-14 10:08:16 -0700

    rename to reflect larger variety of allowed leagues

--- gamesheets.py
+++ gamesheets.py
@@ -1,6 +1,6 @@
 import argparse
 
-from modules import helpers, chl
+from modules import helpers, hockeytech_api
 
 
 allowed_leagues = ['ohl', 'qmjhl', 'whl', 'ushl', 'bchl', 'sijhl, nojhl']
@@ -31,7 +31,7 @@ def main():
             print('Invalid League: {0}'.format(league))
             return
 
-        seasons = helpers.get_json('http://cluster.leaguestat.com/feed/?feed=modulekit&view=seasons&key={0}&fmt=json&client_code={1}&lang=en&league_code=&fmt=json'.format(chl.api_key, chl.league_codes[league]))['SiteKit']['Seasons']
+        seasons = helpers.get_json('http://cluster.leaguestat.com/feed/?feed=modulekit&view=seasons&key={0}&fmt=json&client_code={1}&lang=en&league_code=&fmt=json'.format(hockeytech_api.api_key, hockeytech_api.league_codes[league]))['SiteKit']['Seasons']
 
         selected_seasons = filter(
             lambda season: start_season <= int(season['end_date'][0:4]) <= end_season and season['career'] == '1',
@@ -40,7 +40,7 @@ def main():
 
         for season in reversed(list(selected_seasons)):
             print('Collecting ' + league.upper() + ' ' + season['season_name'] + '...', end='', flush=True)
-            chl.get_season_stats(season, league, results_array)
+            hockeytech_api.get_season_stats(season, league, results_array)
             print(' done.')
 
     helpers.export_array_to_csv(results_array, '{0}-{1}-{2}.csv'.format('-'.join(args.leagues), start_season, end_season))
