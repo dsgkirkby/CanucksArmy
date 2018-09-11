3dff8b3f0641ad188fbc58c1e6679e5828a037d3
Dylan Kirkby <dsgkirkby@gmail.com>
2018-09-10 22:27:16 -0700

    Fix standings scraper

--- modules/standings.py
+++ modules/standings.py
@@ -1,5 +1,8 @@
 import requests
 import html5lib
+from modules import helpers
+
+html_prefix = helpers.html_prefix
 
 NAME = 1
 GAMES = 2
@@ -21,67 +24,57 @@ def get_league_standings(league, season, results_array=None):
     season = str(season)
 
     if results_array is None or len(results_array) == 0:
-        results_array.append(['Team Name', 'League', 'Season', 'Games', 'W', 'L', 'OT', 'GF', 'GA', 'Points', 'PostSeason'])
+        results_array.append(['Team Name', 'League', 'Season', 'Games',
+                              'W', 'L', 'OT', 'GF', 'GA', 'Points', 'PostSeason'])
 
-    standings_url = 'http://www.eliteprospects.com/standings.php?league={0}&startdate={1}'.format(league, str(int(season) - 1))
+    standings_url = 'http://www.eliteprospects.com/standings.php?league={0}&startdate={1}'.format(
+        league, str(int(season) - 1))
     request = requests.get(standings_url)
-
-    # All tag names have this prepended to them
-    html_prefix = '{http://www.w3.org/1999/xhtml}'
     standings_page = html5lib.parse(request.text)
 
     standings_table = standings_page.find(
-        './{0}body/{0}div[2]/{0}div/{0}table[3]/{0}tbody/{0}tr/{0}td[5]/{0}table[3]'.format(html_prefix))
+        './body/section[2]/div/div[1]/div[4]/div[2]/div[1]/div/div[3]/table'.replace('/', '/' + html_prefix))
 
-    teams = standings_table.findall('.//{0}tbody/{0}tr'.format(html_prefix))
+    teams_by_conference = helpers.get_ep_table_rows(standings_table)
 
     """ Parse the team standings table """
 
-    is_first_row = True
+    for conference in teams_by_conference:
+        for team in conference:
+            team_stats = team.findall('.//{0}td'.format(html_prefix))
 
-    for team in teams:
+            name = team_stats[NAME].find(
+                './/{0}span/{0}a'.format(html_prefix)).text.strip()
+            games = team_stats[GAMES].text.strip()
 
-        if is_first_row:
-            is_first_row = False
-            continue
+            def parse_number(node):
+                try:
+                    return int(node.text)
+                except (ValueError, Exception):
+                    return 0
 
-        team_stats = team.findall('.//{0}td'.format(html_prefix))
+            wins = parse_number(team_stats[WINS])
+            losses = parse_number(team_stats[LOSSES])
+            ties = parse_number(team_stats[TIES])
+            ot_wins = parse_number(team_stats[OT_WINS])
+            ot_losses = parse_number(team_stats[OT_LOSSES])
+            goals_for = team_stats[GOALS_FOR].text.strip()
+            goals_against = team_stats[GOALS_AGAINST].text.strip()
+            points = team_stats[POINTS].text.strip()
+            postseason = ''.join(team_stats[POSTSEASON].itertext()).strip()
 
-        """ Exclude subtitle rows """
-        if len(team_stats) < 2:
-            continue
-
-        name = team_stats[NAME].find('.//{0}a'.format(html_prefix)).text
-        games = team_stats[GAMES].text
-
-        def parse_number(node):
-            try:
-                return int(node.text)
-            except (ValueError, Exception):
-                return 0
-
-        wins = parse_number(team_stats[WINS])
-        losses = parse_number(team_stats[LOSSES])
-        ties = parse_number(team_stats[TIES])
-        ot_wins = parse_number(team_stats[OT_WINS])
-        ot_losses = parse_number(team_stats[OT_LOSSES])
-        goals_for = team_stats[GOALS_FOR].text
-        goals_against = team_stats[GOALS_AGAINST].text
-        points = team_stats[POINTS].find('.//{0}strong'.format(html_prefix)).text
-        postseason = ''.join(team_stats[POSTSEASON].itertext()).strip()
-
-        results_array.append([
-            name,
-            league,
-            season,
-            games,
-            wins + ot_wins,
-            losses,
-            ot_losses + ties,
-            goals_for,
-            goals_against,
-            points,
-            postseason
-        ])
+            results_array.append([
+                name,
+                league,
+                season,
+                games,
+                wins + ot_wins,
+                losses,
+                ot_losses + ties,
+                goals_for,
+                goals_against,
+                points,
+                postseason
+            ])
 
     return results_array
