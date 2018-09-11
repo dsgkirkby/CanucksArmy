6aa6adcb18ace97d856c3eeb6a21abb8b21bbaa5
Dylan Kirkby <dsgkirkby@gmail.com>
2018-09-10 20:23:19 -0700

    Fixes for EP redesign

--- modules/draft.py
+++ modules/draft.py
@@ -24,58 +24,62 @@ def get_draft_picks(season, results_array=None, show_extra=False):
     # All tag names have this prepended to them
     html_prefix = '{http://www.w3.org/1999/xhtml}'
     draft_page = html5lib.parse(r.text)
-    # xpath: /html/body/div/table[3]/tbody/tr/td[5]/p[2]/table[1]
     draft_table = draft_page.find(
-        './{0}body/{0}div[2]/{0}div/{0}table[3]/{0}tbody/{0}tr/{0}td[5]/{0}p[2]/{0}table[1]'.format(html_prefix))
+        './body/section[2]/div/div[1]/div[4]/div[3]/div[1]/div[1]/div[3]/table'.replace('/', '/' + html_prefix))
 
     players = draft_table.findall('.//{0}tbody/{0}tr'.format(html_prefix))
     pick_number = 1
-    round_number = 0
 
-    """ Row 0 is the title row """
-    for playerIndex in range(1, len(players)):
-        player = players[playerIndex]
-        columns = player.findall('.//{0}td'.format(html_prefix))
+    rounds = draft_table.findall('.//{}tbody'.format(html_prefix))
 
-        if len(columns[NUMBER].text.strip()) < 1:
-            round_header = columns[TEAM].find('.//{0}strong'.format(html_prefix))
-            if round_header is not None:
-                round_number += 1
-            continue
+    # first tbody is just the header
+    for round_number in range(1, len(rounds)):
+        players = rounds[round_number].findall('.//{}tr'.format(html_prefix))
 
-        player_birthday = ''
+        # Last row is the title row for the next round (unless it's the last round)
+        num_players = len(players) if round_number == len(rounds) - 1 else len(players) - 1
 
-        try:
-            name_link = columns[NAME].find('{0}a'.format(html_prefix))
-            name = name_link.text.strip()
-            position = name_link.find('{0}font'.format(html_prefix)).text.strip()[1:-1]
+        for playerIndex in range(0, num_players):
+            player = players[playerIndex]
+            columns = player.findall('.//{0}td'.format(html_prefix))
 
-            if show_extra:
-                player_request = requests.get('http://www.eliteprospects.com/' + name_link.attrib['href'])
-                player_page = html5lib.parse(player_request.text)
+            player_birthday = ''
 
-                player_birthday = player_page.find('./{0}body/{0}div[2]/{0}table[3]/{0}tbody/{0}tr/{0}td[5]/{0}p/{0}table[2]/{0}tbody/{0}tr/{0}td[1]/{0}table/{0}tbody/{0}tr[1]/{0}td[2]/{0}a'.format(html_prefix)).text
-        except AttributeError:
-            name = 'No selection made'
-            position = 'N/A'
+            try:
+                name_link = columns[NAME].find('./{0}span/{0}a'.format(html_prefix))
+                name_raw = name_link.text.strip()
+                name_parts = name_raw.split('(')
+                name = name_parts[0].strip()
+                position = name_parts[1][:-1]
 
-        results_array.append([
-            season,
-            round_number,
-            pick_number,
-            columns[TEAM].text,
-            name,
-            position,
-            columns[SEASONS].text,
-            columns[GAMES].text or '',
-            columns[GOALS].text or '',
-            columns[ASSISTS].text or '',
-            columns[POINTS].text or '',
-            columns[PIM].text or '',
-            player_birthday
-        ])
+                if show_extra:
+                    player_request = requests.get(name_link.attrib['href'])
+                    player_page = html5lib.parse(player_request.text)
 
-        pick_number += 1
+                    player_birthday = player_page.find(
+                        './body/section[2]/div/div[1]/div[4]/div[1]/div[1]/div[2]/section/div[4]/div[1]/div[1]/ul/li[1]/div[2]/a'.replace('/', '/' + html_prefix)
+                    ).text.strip()
+            except AttributeError:
+                name = 'No selection made'
+                position = 'N/A'
+
+            results_array.append([
+                season,
+                round_number,
+                pick_number,
+                columns[TEAM].find('./{}a'.format(html_prefix)).text,
+                name,
+                position,
+                columns[SEASONS].text.strip(),
+                columns[GAMES].text.strip() or '',
+                columns[GOALS].text.strip() or '',
+                columns[ASSISTS].text.strip() or '',
+                columns[POINTS].text.strip() or '',
+                columns[PIM].text.strip() or '',
+                player_birthday
+            ])
+
+            pick_number += 1
 
     return results_array
 
