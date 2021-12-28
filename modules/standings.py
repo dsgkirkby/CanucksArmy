13e9ce721f657049d8f4e65cf43bb08d3614641f
VivalaSedinery <davis.jeremy89@gmail.com>
2021-12-28 14:38:58 -0800

    Update standings.py
    Revise Standings table location.

--- modules/standings.py
+++ modules/standings.py
@@ -34,7 +34,7 @@ def get_league_standings(league, season, results_array=None):
     standings_page = html5lib.parse(request.text)
 
     standings_table = standings_page.find(
-        './body/section[2]/div/div[1]/div[4]/div[2]/div[1]/div/div[3]/table'.replace('/', '/' + html_prefix))
+        './body/section/div/div/div[1]/div/div[2]/table'.replace('/', '/' + html_prefix))
 
     teams_by_conference = helpers.get_ep_table_rows(standings_table)
 
